0a27c205db40440df91dd6bebba2b1e3c03c8f3e
janiap <jani.peltonen3@metropolia.fi>
2021-04-13 14:59:25 +0300

    muutoksia

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
-import Vue from 'vue'
+import { createApp } from 'vue';
 import App from './App.vue'
 
-const app = Vue.createApp(App);
+const app = createApp(App);
 
-app.mount("app");
+app.mount("#app");

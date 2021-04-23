370d39192392150e2f400b29eb09d9d0d6b544cc
janiap <jani.peltonen3@metropolia.fi>
2021-04-23 12:51:24 +0300

    Vuex

--- src/main.js
+++ src/main.js
@@ -2,10 +2,23 @@ import { createApp } from 'vue';
 import App from './App.vue'
 import router from './router.js';
 
+import { createStore } from 'vuex';
+
+const store = createStore( {
+    state(){
+        return {
+
+        }
+    },
+    mutations: {},
+    actions: {},
+    getters: {}
+});
+
 import TheHeader from "./components/Layout/TheHeader.vue";
 
 const app = createApp(App);
-
+app.use(store);
 app.component('the-header', TheHeader);
 
 app.use(router);

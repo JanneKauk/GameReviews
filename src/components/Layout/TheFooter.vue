4c40ecfd273b111383150d42a02f412bdbf3e8f9
janiap <jani.peltonen3@metropolia.fi>
2021-05-03 23:49:55 +0300

    Footer and other improvements

---
+++ src/components/Layout/TheFooter.vue
@@ -0,0 +1,16 @@
+<template>
+    <footer></footer>
+</template>
+
+
+<style scoped>
+
+/* footer {
+  display: block;
+  min-height: calc(100% - 1rem);
+  height: 100px; 
+  background-color: red; 
+  position: relative;
+} */
+
+</style>
